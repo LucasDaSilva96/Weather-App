c1847d0bf4d350b5937c48b92fecad3a6346a496
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-29 21:13:40 +0200

    Create forecast animation + add style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { activateShowBarAni, activateHideSearchBar } from "./animation.js";
 
-document.querySelector("body").classList.add("cold-weather");
+document.querySelector("body").classList.add("warm-weather");
 
 window.addEventListener("DOMContentLoaded", function () {
   activateShowBarAni();
